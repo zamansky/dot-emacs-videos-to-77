c7d4a3eace8b3fb6cd3ce67eff08bd97321b8e26
Mike Zamansky <zamansky@gmail.com>
2019-10-06 16:05:32 -0400

    first commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+ ;; (add-to-list 'package-archives
+ ;; 	     '("gnu" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+
+;; ------------------------------------------------------
+
+
