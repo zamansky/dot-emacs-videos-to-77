0d1ec21eeff59bb6924d5c6922306c932656b986
Mike Zamansky <zamansky@gmail.com>
2019-12-01 08:30:14 -0500

    added smartparens keybinding

--- init.el
+++ init.el
@@ -22,7 +22,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9fe9a86557144c43f9008daaf4bbee29498e30a3d957f6cf42b63b35cf598fd1" default))
+   '("13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "9fe9a86557144c43f9008daaf4bbee29498e30a3d957f6cf42b63b35cf598fd1" default))
+ '(org-agenda-files
+   '("~/Sync/orgfiles/i.org" "~/Sync/orgfiles/gcal.org" "~/Sync/orgfiles/soe-cal.org"))
  '(org-confirm-babel-evaluate nil)
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Sync/orgfiles")
@@ -32,7 +34,7 @@
  '(org-startup-folded 'overview)
  '(org-startup-indented t)
  '(package-selected-packages
-   '(yasnippet-snippets restclient grip-mode markdown-preview-eww projectile doom-modeline try faff-theme faff lsp-java default-text-scale lsp-ui-doc lsp-ui lsp-clangd virtualenvwrapper company-lsp company lsp-mode circe-notifications circe org-gcal mu4e-alert htmlize org-bullets which-key posframe counsel use-package)))
+   '(forge org-msg yasnippet-snippets restclient grip-mode markdown-preview-eww projectile doom-modeline try faff-theme faff lsp-java default-text-scale lsp-ui-doc lsp-ui lsp-clangd virtualenvwrapper company-lsp company lsp-mode circe-notifications circe org-gcal mu4e-alert htmlize org-bullets which-key posframe counsel use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
