1476246a84022219703a443ab42201b109612b2f
Mike Zamansky <zamansky@gmail.com>
2020-01-29 11:01:13 -0500

    added mbsync note

--- init.el
+++ init.el
@@ -13,18 +13,11 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
 
-;; ------------------------------------------------------
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "9fe9a86557144c43f9008daaf4bbee29498e30a3d957f6cf42b63b35cf598fd1" default))
- '(org-agenda-files
-   '("~/Sync/orgfiles/i.org" "~/Sync/orgfiles/gcal.org" "~/Sync/orgfiles/soe-cal.org"))
  '(org-confirm-babel-evaluate nil)
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Sync/orgfiles")
@@ -34,7 +27,7 @@
  '(org-startup-folded 'overview)
  '(org-startup-indented t)
  '(package-selected-packages
-   '(forge org-msg yasnippet-snippets restclient grip-mode markdown-preview-eww projectile doom-modeline try faff-theme faff lsp-java default-text-scale lsp-ui-doc lsp-ui lsp-clangd virtualenvwrapper company-lsp company lsp-mode circe-notifications circe org-gcal mu4e-alert htmlize org-bullets which-key posframe counsel use-package)))
+   '(ivy-rich focus multiple-cursors company-restclient restclient default-text-scale grip-mode dumb-jump cider irony-eldoc company-irony virtualenvwrapper company-lsp company projectile ripgrep lsp-mode yasnippet-classic-snippets yasnippet-snippets yasnippet circe-notifications circe org-gcal mu4e-alert rainbow-delimiters smartparens org-msg htmlize org-bullets forge git-gutter-fringe git-timemachine magit origami hydra flycheck counsel hlinum pcre2el which-key iedit posframe try ace-window aggressive-indent hungry-delete expand-region doom-modeline faff-theme alect-themes ample-zen-theme ample-theme gruvbox-theme monokai-theme zenburn-theme moe-theme cloud-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
